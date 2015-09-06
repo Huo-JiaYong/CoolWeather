6eb96f0454933de93037352ab3adc2089d510ee8
Jyong <h786672084@gmail.com>
2015-09-06 17:09:21 +0800

    新增数据库帮助类，已及各表对应的实体类

---
+++ app/src/main/java/com/jyong/coolweather/db/CoolWeatherDB.java
@@ -0,0 +1,160 @@
+package com.jyong.coolweather.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.jyong.coolweather.model.City;
+import com.jyong.coolweather.model.County;
+import com.jyong.coolweather.model.Province;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 加勇 on 2015/9/6.
+ */
+public class CoolWeatherDB {
+    public static final String DB_NAME = "cool_weather";
+    public static final int VERSION = 1;
+    private static CoolWeatherDB coolWeatherDB;
+    private SQLiteDatabase db;
+
+
+    private CoolWeatherDB(Context context) {
+        CoolWeatherOpenHelper helper = new CoolWeatherOpenHelper(context, DB_NAME, null, VERSION);
+        db = helper.getWritableDatabase();
+    }
+
+    /**
+     * 获取CoolWeatherDB的实例
+     *
+     * @param context
+     * @return
+     */
+    public synchronized static CoolWeatherDB getInstance(Context context) {
+        if (coolWeatherDB == null) {
+            coolWeatherDB = new CoolWeatherDB(context);
+        }
+        return coolWeatherDB;
+    }
+
+    /**
+     * 保存province对象到数据库
+     *
+     * @param province
+     */
+    public void saveProvince(Province province) {
+        if (province != null) {
+            ContentValues values = new ContentValues();
+            values.put("province_name", province.getProvinceName());
+            values.put("province_code", province.getProvinceCode());
+            db.insert("Province", null, values);
+        }
+    }
+
+    /**
+     * 获取全国省份的信息
+     *
+     * @return
+     */
+    public List<Province> loadProvinces() {
+        List<Province> list = new ArrayList<Province>();
+        Cursor cursor = db.query("Province", null, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            do {
+                Province province = new Province();
+                province.setId(cursor.getColumnIndex("id"));
+                province.setProvinceName(cursor.getString(cursor.getColumnIndex("province_name")));
+                province.setProvinceCode(cursor.getString(cursor.getColumnIndex("province_code")));
+                list.add(province);
+            } while (cursor.moveToNext());
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+        return list;
+    }
+
+    /**
+     * 保存city到数据库
+     *
+     * @param city
+     */
+    public void saveCity(City city) {
+        if (city != null) {
+            ContentValues values = new ContentValues();
+            values.put("city_name", city.getCityName());
+            values.put("city_code", city.getCityCode());
+            values.put("province_id", city.getProvinceID());
+            db.insert("City", null, values);
+        }
+    }
+
+    /**
+     * 获取某省下面的城市信息
+     *
+     * @param provinceID
+     * @return
+     */
+    public List<City> loadCities(int provinceID) {
+        List<City> list = new ArrayList<City>();
+        Cursor cursor = db.query("City", null, "province_id = ?", new String[]{String.valueOf(provinceID)}, null, null, null);
+        if (cursor.moveToFirst()) {
+            do {
+                City city = new City();
+                city.setId(cursor.getInt(cursor.getColumnIndex("city_id")));
+                city.setCityName(cursor.getString(cursor.getColumnIndex("city_name")));
+                city.setCityCode(cursor.getString(cursor.getColumnIndex("city_code")));
+                city.setProvinceID(provinceID);
+                list.add(city);
+            } while (cursor.moveToNext());
+        }
+        if (cursor != null) {
+            cursor.close();
+        }
+        return list;
+    }
+
+    /**
+     * 保存county到数据库
+     *
+     * @param county
+     */
+    public void saveCounty(County county) {
+        if (county != null) {
+            ContentValues values = new ContentValues();
+            values.put("county_name", county.getCountyName());
+            values.put("county_code", county.getCountyCode());
+            values.put("city_id", county.getCityID());
+            db.insert("County", null, values);
+        }
+    }
+
+    /**
+     * 获取cityID下面的县城信息
+     *
+     * @param cityID
+     * @return
+     */
+    public List<County> loadCounties(int cityID) {
+        List<County> list = new ArrayList<County>();
+        Cursor cursor = db.query("County", null, "city_id = ?", new String[]{String.valueOf(cityID)}, null, null, null);
+        if (cursor.moveToFirst()){
+            do {
+                County county = new County();
+                county.setId(cursor.getInt(cursor.getColumnIndex("county_id")));
+                county.setCountyName(cursor.getString(cursor.getColumnIndex("county_name")));
+                county.setCountyCode(cursor.getString(cursor.getColumnIndex("county_code")));
+                county.setCityID(cityID);
+                list.add(county);
+            } while(cursor.moveToNext());
+        }
+        if (cursor != null){
+            cursor.close();
+        }
+        return list;
+    }
+
+}
---
+++ app/src/main/java/com/jyong/coolweather/model/City.java
@@ -0,0 +1,43 @@
+package com.jyong.coolweather.model;
+
+/**
+ * Created by  on 2015/9/6.
+ */
+public class City {
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private int provinceID;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceID() {
+        return provinceID;
+    }
+
+    public void setProvinceID(int provinceID) {
+        this.provinceID = provinceID;
+    }
+}
